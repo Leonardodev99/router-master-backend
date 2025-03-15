8a681e613cec899071fe56dc4e8256cc2de1c8be
Leonardo Tiago <pequenotiago@gmail.com>
2025-03-15 19:45:31 +0100

    Gerando o jwt do usuário

---
+++ src/controllers/TokenController.js
@@ -0,0 +1,44 @@
+import User from '../models/User';
+import jwt from 'jsonwebtoken';
+import bcryptjs from 'bcryptjs';
+
+
+class TokenController {
+  async store(req, res) {
+    const { email = '', password = ''} = req.body;
+
+
+    if(!email || !password) {
+      return res.status(400).json({
+        errors: ['Invalid credentials'],
+      });
+    }
+
+    const user = await User.findOne({ where: { email }});
+
+
+    if(!user) {
+      return res.status(400).json({
+        errors: ['User does not exist'],
+      });
+    }
+
+     // Verificar a senha
+     if (!(await bcryptjs.compare(password, user.password_hash))) {
+      return res.status(401).json({
+        errors: ['Invalid password']
+      });
+    }
+
+    const { id } = user;
+    const tokenUser = jwt.sign({ id, email }, process.env.TOKEN_SECRET, {
+      expiresIn: process.env.TOKEN_EXPERATION,
+    });
+
+    return res.json({ tokenUser });
+
+
+  }
+}
+
+export default new TokenController();
